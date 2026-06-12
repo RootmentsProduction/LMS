2229ef819cf3736f672ec70591f06ddc2884dec4
Rootments IT <abhiramskumar75@gmail.com>
2026-06-12 16:42:02 +0530

    add aws api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ const baseUrl = {
   // Automatically switch between local development and production Render URL!
   baseUrl: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? "http://localhost:7000/"
-    : "https://lms-1-lavs.onrender.com/",
+    : "https://lms.brynexapparels.in/",
 };
 
 /**
