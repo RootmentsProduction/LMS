9181f7f84368d5fb390949452f4cc3ea21c676c6
Rootments I <techrootments@gmail.com>
2026-08-15 10:24:51 +0530

    fix(api): set production URL to https://lms.brynexapparels.in/

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ const baseUrl = {
   // https://lms-testenv-v0w5.onrender.com/
   baseUrl: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? "http://localhost:7000/"
-    : "https://lms-testenv-v0w5.onrender.com/",
+    : "https://lms.brynexapparels.in/",
 };
 
 /**
